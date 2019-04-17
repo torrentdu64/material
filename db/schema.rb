328273ad280603eca70c7a534d418d4f331f953c
torrent benoit <torrentdu64@gmail.com>
2019-04-17 16:16:36 +1200

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190417040832) do
+ActiveRecord::Schema.define(version: 20190417041510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20190417040832) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "product_id"
+    t.bigint "chat_room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+    t.index ["product_id"], name: "index_messages_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -40,4 +50,6 @@ ActiveRecord::Schema.define(version: 20190417040832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "chat_rooms"
+  add_foreign_key "messages", "products"
 end
