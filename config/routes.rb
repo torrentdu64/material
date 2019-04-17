aef1976c1265607c54f24171141dd9bb222da8db
torrent benoit <torrentdu64@gmail.com>
2019-04-17 17:13:22 +1200

    add from https://github.com/lewagon/action_cable_chat/tree/actioncable-setup/app

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
 
 
 
+
   devise_for :users
   root to: 'pages#home'
-   resources :products
+   resources :products, only: [:index, :show]
    resources :chat_rooms, only: :show do
      resources :messages, only: :create
    end
-   resources :messages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
